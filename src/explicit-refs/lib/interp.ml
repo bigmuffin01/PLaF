6765b6ea1b3c675d4eaddc614e016ef677d8d14b
bigmuffin01 <tonyyi5505@gmail.com>
2026-04-11 09:02:24 -0400

    HW 4 part 1 complete

--- src/explicit-refs/lib/interp.ml
+++ src/explicit-refs/lib/interp.ml
@@ -97,13 +97,68 @@ let rec eval_expr : expr -> exp_val ea_result = fun e ->
     sequence (List.map eval_expr es) >>= fun l ->
     return (List.hd (List.rev l))
   | Unit -> return UnitVal
-  (** HOMEWORK 4 *)
+
+  (* ==== HOMEWORK 4 ==== *)
+  (* Part 1 *)
+  | IsEqual(e1,e2) -> (* check that evaluation of e1, e2 are NumVals *)
+    eval_expr e1 >>=
+    int_of_numVal >>= fun n1 ->
+    eval_expr e2 >>=
+    int_of_numVal >>= fun n2 ->
+    return @@ BoolVal(n1 = n2)
+  | IsGT(e1, e2) -> (* check that evaluation of e1, e2 are NumVals *)
+    eval_expr e1 >>=
+    int_of_numVal >>= fun n1 ->
+    eval_expr e2 >>=
+    int_of_numVal >>= fun n2 ->
+    return @@ BoolVal(n1 > n2)
+  | IsLT (e1, e2) -> (* check that evaluation of e1 , e2 are NumVals *)
+    eval_expr e1 >>=
+    int_of_numVal >>= fun n1 ->
+    eval_expr e2 >>=
+    int_of_numVal >>= fun n2 ->
+    return @@ BoolVal(n1 < n2)
+  | IsNumber(e) ->
+    eval_expr e >>= fun ev ->
+    (match ev with
+    | NumVal _ -> return @@ BoolVal true
+    | _ -> return @@ BoolVal false)
+  | Record(fs)->
+    eval_fields fs >>= fun evs ->
+    return ( RecordVal ( addIds fs evs ))
+  | Proj(e,id) ->
+    eval_expr e >>=
+    fields_of_recordVal >>= fun n ->
+    (match (List.assoc id n) with
+    | (true, RefVal v) ->
+      Store.deref g_store v
+    | (false, v) -> return v)
+  | SetField(e1,id,e2) ->
+    eval_expr e1 >>=
+    fields_of_recordVal >>= fun n1 ->
+    eval_expr e2 >>= fun n2 ->
+    (match (List.assoc id n1) with
+    | (true, RefVal v) ->
+      Store.set_ref g_store v n2 >>= fun _ ->
+      return UnitVal
+    | (false, v) -> error "Field not mutable")
+
   | Debug(_e) ->
     string_of_env >>= fun str_env ->
     let str_store = Store.string_of_store string_of_expval g_store 
     in (print_endline (str_env^"\n"^str_store);
     error "Reached breakpoint")
   | _ -> failwith ("Not implemented: "^string_of_expr e)
+and
+eval_fields fs =
+  match fs with
+  | [] -> return []
+  | (_id, (is_mutable,e))::t ->
+    eval_expr e >>= fun ev ->
+    eval_fields t >>= fun evs ->
+    if is_mutable
+    then return ((RefVal (Store.new_ref g_store ev))::evs)
+    else return (ev::evs)
 
 let eval_prog (AProg(_,e)) =
   eval_expr e         
@@ -113,5 +168,8 @@ let interp (s:string) : exp_val result =
   let c = s |> parse |> eval_prog
   in run c
 
-
-
+let interpf (s:string) : exp_val result =
+  let s = String.trim s
+  in let file_name =
+    match String.index_opt s '.' with None -> s^".exr" | _ -> s
+    in interp @@ read_file file_name
